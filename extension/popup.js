4d7eacec650e2f167f5aace03c3f2163caa5131c
tonakai-s <rdorneles64@gmail.com>
2025-11-27 22:34:42 -0300

    add tungstenite and add a basic js ext setup

--- extension/popup.js
+++ extension/popup.js
@@ -1,9 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("myButton");
+let socket = undefined;
+let pingInterval = undefined;
+const btn = document.getElementById("myButton");
 
-  btn.addEventListener("click", () => {
-    console.log("Button clicked!");
-    alert("Button clicked!");
-    fetch("http://127.0.0.1:8080/");
-  });
+socket = new WebSocket("ws://127.0.0.1:9001");
+socket.addEventListener("open", () => {
+	console.log("connection openned");
+	let counter = 0;
+	pingInterval = setInterval(() => {
+		socket.send("ping " + counter);
+		counter++;
+	}, 1000);
+});
+socket.addEventListener("close", () => {
+	console.log("connection closed");
+});
+socket.addEventListener("message", (e) => {
+	console.log(e.data);
+});
+socket.addEventListener("error", (e) => {
+	console.log(e);
 });
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-fn main() {
-    let server = tiny_http::Server::http("127.0.0.1:8080").unwrap();
-    loop {
-        match server.recv() {
-            Ok(req) => println!("Request: {req:?}"),
-            Err(err) => println!("Err: {err:?}")
-        }
+use std::net::TcpListener;
+use std::thread::spawn;
+use tungstenite::accept;
+
+fn main () {
+    let server = TcpListener::bind("127.0.0.1:9001").unwrap();
+    for stream in server.incoming() {
+        spawn (move || {
+            let mut websocket = accept(stream.unwrap()).unwrap();
+            println!("socket connected");
+            loop {
+                let msg = websocket.read().unwrap();
+
+                if msg.is_binary() || msg.is_text() {
+                    websocket.send(msg).unwrap();
+                }
+            }
+        });
     }
 }
+
