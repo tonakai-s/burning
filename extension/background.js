e698cf2c903bf89918032e7dfa319026f9b07ff2
tonakai-s <rdorneles64@gmail.com>
2025-12-02 22:05:25 -0300

    page -> extension connection implementation

--- extension/background.js
+++ extension/background.js
@@ -1,18 +1,8 @@
-self.addEventListener('message', event => {
-	console.log('would start a socket')
+browser.runtime.onMessage.addListener(async (msg, sender) => {
+    const tabs = await browser.tabs.query({currentWindow: true});
+    let tab = tabs.find(t => t.url.includes("wikipedia.org"));
+    if(tab){
+        await browser.tabs.reload(tab.id, { bypassCache: true });
+    }
+    console.log("Got from page:", msg);
 });
-//browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-//    console.log("message received", message)
-//    sendResponse({response: "msg recv by extension" })
-//})
-//browser.runtime.onMessageExternal.addListener(
-//      function(request, sender, sendResponse) {
-//        if (request.type === "FROM_WEBPAGE") {
-//          console.log("Message received from webpage:", request.data);
-//          // Process the message and send a response back to the webpage if needed
-//          sendResponse({ status: "success", message: "Received your message!" });
-//        }
-//        // Return true to indicate that you want to send a response asynchronously
-//        return true;
-//      }
-//    );
--- extension/content-script.js
+++ extension/content-script.js
@@ -1,38 +1,6 @@
-let socket = undefined;
-let pingInterval = undefined;
-const btn = document.getElementById("myButton");
-btn.addEventListener('click', ev => {
+window.addEventListener("message", event => {
+    if (event.source !== window) return;
+    if (event.data?.from !== "burning") return;
 
-    browser.tabs.create({ url: 'burning.html' });
+    browser.runtime.sendMessage(event.data);
 });
-
-// In your content-script.js
-console.log("starting ext")
-
-window.addEventListener("message", (event) => {
-    console.log("Content script received message: \"" + event.data.message + "\"");
-  if (event.source == window &&
-      event.data &&
-      event.data.direction == "from-page-script") {
-    console.log("Content script received message: \"" + event.data.message + "\"");
-  }
-});
-
-//socket = new WebSocket("ws://127.0.0.1:9001");
-//socket.addEventListener("open", () => {
-//	console.log("connection openned");
-//	let counter = 0;
-//	pingInterval = setInterval(() => {
-//		socket.send("ping " + counter);
-//		counter++;
-//	}, 1000);
-//});
-//socket.addEventListener("close", () => {
-//	console.log("connection closed");
-//});
-//socket.addEventListener("message", (e) => {
-//	console.log(e.data);
-//});
-//socket.addEventListener("error", (e) => {
-//	console.log(e);
-//});
